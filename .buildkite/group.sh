daded268ffb74f29af0d3f911160fab8bd88be3d
mkmrgn <mike@morgan.ms>
2021-11-16 16:33:29 -0800

    steps pipeline

---
+++ .buildkite/group.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+name=$(buildkite-agent meta-data get name)
+loops=$(buildkite-agent meta-data get loops)
+
+echo "  - group: ":curly_loop: group step"
+echo "    key: groupstep"
+echo "    steps:"
+for((i=0;i<loops;i++));
+do
+  cat <<YAML
+            - label: "$1"
+              command: "echo 'hello $name'"
+YAML
+done
+  
