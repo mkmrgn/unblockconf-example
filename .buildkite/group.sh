2b1660126b582e590daef80fe15b3d988a42e158
mkmrgn <mike@morgan.ms>
2021-11-16 16:50:13 -0800

    another typo

--- .buildkite/group.sh
+++ .buildkite/group.sh
@@ -5,14 +5,14 @@ set -e
 name=$(buildkite-agent meta-data get name)
 loops=$(buildkite-agent meta-data get loops)
 
-echo "  - group: ":curly_loop: group step"
+echo "  - group: ':curly_loop: group step'"
 echo "    key: groupstep"
 echo "    steps:"
 for((i=0;i<loops;i++));
 do
   cat <<YAML
-            - label: "$1"
-              command: "echo 'hello $name'"
+            - label: "$i"
+              command: echo "hello $name"
 YAML
 done
   
